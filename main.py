786e6489c61d79fbd47b6f3d5c2ba1e3d03016d4
Rehanabbaxi <rehan020345@gmail.com>
2025-08-13 23:12:33 +0500

    added code for accesing DataManager class acces

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import *
+
+sheet_endpoint  = 'https://api.sheety.co/48150efc13c26d390381a34f7aa31cef/copyOfFlightDeals/prices'
+
+data_manager = DataManager(sheet_endpoint)
+data = data_manager.get_sheet_data()
+print(data)
